d3406c0acd8eb277570699fe7e74eab34b621187
Celso Taliatelli <celsotaliatelli@hotmail.com>
2021-05-09 20:25:03 -0300

    aplicando estrutura de array para mudar formularios

--- src/components/FormularioCadastro/DadosUsuario.jsx
+++ src/components/FormularioCadastro/DadosUsuario.jsx
@@ -1,19 +1,29 @@
 import { Button, TextField } from '@material-ui/core';
 import React from 'react';
 
-function DadosUsuario(){
+function DadosUsuario({aoEnviar}){
     return(
-        <form>
+        <form onSubmit={(event) => {
+            event.preventDefault();
+            aoEnviar();
+        }}>
             <TextField 
             id="email" 
             label="email" type="email" 
             variant="outlined"
             margin="normal"
-            fullWidth/>
-            <TextField id="senha" label="senha" type="password" 
-            variant="outlined"
-            margin="normal"
-            fullWidth/>
+            fullWidth
+            required
+            />
+            <TextField 
+                id="senha" 
+                label="senha" 
+                type="password" 
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                required
+            />
             <Button type="submit" variant="contained" color="primary">Cadastrar</Button>
         </form>
     );
--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,17 +1,28 @@
-import React from "react";
+import { Typography } from "@material-ui/core";
+import React,{useState} from "react";
 import DadosEntrega from "./DadosEntrega";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
 
 function FormularioCadastro({aoEnviar, validarCPF}) {
+  const[etapaAtual,setEtapaAtual]  = useState(0);
+
+  const formularios = [
+    <DadosUsuario aoEnviar={proximo}/>,
+    <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF}/>,
+    <DadosEntrega aoEnviar={aoEnviar}/>
+  ]
+
+  function proximo(){
+    setEtapaAtual(etapaAtual+1);
+  }
   
   return (
     <>
-    <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF}/>
-    <DadosUsuario />
-    <DadosEntrega />
+      {formularios([etapaAtual])}
     </>
   );
 }
+
 
 export default FormularioCadastro;
