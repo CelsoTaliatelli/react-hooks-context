fa4e7e3e8cc8c13253c694f7f1df7c3cd53360ca
Celso Taliatelli <celsotaliatelli@hotmail.com>
2021-05-11 21:57:35 -0300

    Add Stepper

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,4 +1,8 @@
 import { Typography } from "@material-ui/core";
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+
 import React,{useEffect, useState} from "react";
 import DadosEntrega from "./DadosEntrega";
 import DadosPessoais from "./DadosPessoais";
@@ -7,15 +11,18 @@ import DadosUsuario from "./DadosUsuario";
 function FormularioCadastro({aoEnviar, validarCPF}) {
   const[etapaAtual,setEtapaAtual]  = useState(0);
   const[dadosColetados,setDadosColetados] = useState({});
-
+ 
   useEffect(() => {
-    console.log(dadosColetados);
+    if(etapaAtual === formularios.length-1){
+        aoEnviar(dadosColetados); 
+    }
   })
 
   const formularios = [
     <DadosUsuario aoEnviar={coletarDados}/>,
     <DadosPessoais aoEnviar={coletarDados} validarCPF={validarCPF}/>,
-    <DadosEntrega aoEnviar={aoEnviar}/>
+    <DadosEntrega aoEnviar={coletarDados}/>,
+    <Typography variant="h5">Obrigado pelo Cadastro!</Typography>
   ]
 
   function coletarDados(dados){
@@ -29,6 +36,12 @@ function FormularioCadastro({aoEnviar, validarCPF}) {
   
   return (
     <>
+       <Stepper activeStep={etapaAtual}> 
+        <Step><StepLabel>Login</StepLabel></Step>
+        <Step><StepLabel>Dados Pessoais</StepLabel></Step>
+        <Step><StepLabel>Entrega</StepLabel></Step>
+        <Step><StepLabel>Finalizar</StepLabel></Step>
+      </Stepper>
       {formularios[etapaAtual]}
     </>
   );
