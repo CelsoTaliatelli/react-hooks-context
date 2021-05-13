8a330cc66ab2974339fa22d7ce670a43d9c5db8d
Celso Taliatelli <celsotaliatelli@hotmail.com>
2021-05-13 10:10:30 -0300

    utilizando context Provider

--- src/components/FormularioCadastro/DadosPessoais.jsx
+++ src/components/FormularioCadastro/DadosPessoais.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState,useContext } from "react";
 import { TextField, Button, Switch, FormControlLabel } from "@material-ui/core";
+import ValidacoesCadastro from "../../contexts/ValidacoesCadastro";
 
-function DadosPessoais({aoEnviar, validacoes}) {
+function DadosPessoais({aoEnviar}) {
   const [nome, setNome] = useState("");
   const [sobrenome, setSobrenome] = useState("");
   const [cpf, setCpf] = useState("");
@@ -10,6 +11,7 @@ function DadosPessoais({aoEnviar, validacoes}) {
   
   const [erros, setErros] = useState({cpf:{valido:true, texto:""}})
 
+  const validacoes = useContext(ValidacoesCadastro);
   function validarCampos(event){
     const{name,value} = event.target;
     const novoEstado = {...erros}
